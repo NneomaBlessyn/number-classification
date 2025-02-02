e6d6338a141e03de1a76d8ccc221107fba2340d3
Nneoma Nwachukwu <nneomablessyn@gmail.com>
2025-02-02 22:13:09 +0100

    Fix issue with properties list

--- src/main/java/com/hng12/number_classification/service/implementation/NumberServiceImpl.java
+++ src/main/java/com/hng12/number_classification/service/implementation/NumberServiceImpl.java
@@ -11,8 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -45,7 +45,7 @@ public class NumberServiceImpl implements NumberService {
         boolean isArmstrong = checkArmstrong(number);
         boolean isEven = (number % 2 == 0);
 
-        Set<String> properties = new HashSet<>();
+        List<String> properties = new ArrayList<>();
         if (isArmstrong && isEven) {
             properties.add("armstrong");
             properties.add("even");
